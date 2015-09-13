750c3cf8225bd4d0bff0f9973680de5708f3f64a
Armand Grillet <armand.grillet@outlook.com>
2015-09-13 16:21:00 +0200

    UI updated

--- front.js
+++ front.js
@@ -1,23 +1,9 @@
 'use strict';
 
 window.onload = function() {
-    document.getElementById('import').onclick = importEasyList;
     document.getElementById('process').onclick = runEngine;
     document.getElementById('save').onclick = saveRules;
 };
-
-function importEasyList() {
-    var req = new XMLHttpRequest();
-    req.open('GET', 'https://easylist-downloads.adblockplus.org/easylist.txt', false); 
-    req.send(null);
-    if (req.status == 200) {
-        setImportedText(req.responseText);
-    }
-}
-
-function setImportedText(text) {
-    document.getElementById('standard-list').value = text;
-}
 
 function runEngine() {
     var standardList = document.getElementById('standard-list').value.split('\n');
@@ -43,3 +29,29 @@ function saveRules() {
     var blob = new Blob([document.getElementById('processed-list').innerHTML], {type: "text/plain;charset=utf-8"});
     saveAs(blob, "contentBlockerRules.json");
 }
+
+var J50Npi = {  
+    currentScript: null,  
+    getJSON: function(url, data, callback) {
+      var src = url + (url.indexOf("?")+1 ? "&" : "?");
+      var head = document.getElementsByTagName("head")[0];
+      var newScript = document.createElement("script");
+      var params = [];
+      var param_name = ""
+
+      this.success = callback;
+
+      data["callback"] = "J50Npi.success";
+      for(param_name in data){  
+          params.push(param_name + "=" + encodeURIComponent(data[param_name]));  
+      }
+      src += params.join("&")
+
+      newScript.type = "text/javascript";  
+      newScript.src = src;
+
+      if(this.currentScript) head.removeChild(currentScript);
+      head.appendChild(newScript); 
+    },
+    success: null
+}; 
