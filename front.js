e3a9fc87a73e019c763ad3bbfa54293032bd6802
Armand Grillet <armand.grillet@outlook.com>
2015-09-13 16:22:03 +0200

    J50Npi removed

--- front.js
+++ front.js
@@ -29,29 +29,3 @@ function saveRules() {
     var blob = new Blob([document.getElementById('processed-list').innerHTML], {type: "text/plain;charset=utf-8"});
     saveAs(blob, "contentBlockerRules.json");
 }
-
-var J50Npi = {  
-    currentScript: null,  
-    getJSON: function(url, data, callback) {
-      var src = url + (url.indexOf("?")+1 ? "&" : "?");
-      var head = document.getElementsByTagName("head")[0];
-      var newScript = document.createElement("script");
-      var params = [];
-      var param_name = ""
-
-      this.success = callback;
-
-      data["callback"] = "J50Npi.success";
-      for(param_name in data){  
-          params.push(param_name + "=" + encodeURIComponent(data[param_name]));  
-      }
-      src += params.join("&")
-
-      newScript.type = "text/javascript";  
-      newScript.src = src;
-
-      if(this.currentScript) head.removeChild(currentScript);
-      head.appendChild(newScript); 
-    },
-    success: null
-}; 
