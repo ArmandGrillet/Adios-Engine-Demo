108ecd89fc7c5677c99bbcc7c1f8aa3e0f5de7e7
Armand Grillet <armand.grillet@outlook.com>
2015-09-14 20:52:30 +0200

    Refactorisation

--- front.js
+++ front.js
@@ -8,19 +8,8 @@ window.onload = function() {
 function runEngine() {
     var standardList = document.getElementById('standard-list').value.split('\n');
     var json = [];
-    for (var line of standardList) {
-        if (isRule(line)) {
-            var rule = parseRule(line);
-            if (rule !== undefined) {
-                if (rule instanceof Array) { // Multiple rules, exceptionnal case due to the exclusivity of if-domain and unless-domain
-                    for (var realRule of rule) {
-                        json.push(realRule);
-                    }
-                } else {
-                    json.push(rule);
-                }
-            }
-        }
+    for (var rule of parseRules(standardList)) {
+        json.push(rule);
     }
     document.getElementById('processed-list').innerHTML = JSON.stringify(json);
 }
